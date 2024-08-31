addf32907c76e33b5807c0655b986a78c996d91c
Administrator <664130988@qq.com>
2024-08-31 16:54:17 +0800

    修改字体大小为40

--- src/test/java/com/app/util/upload_video/UploadVideoUtilTest.java
+++ src/test/java/com/app/util/upload_video/UploadVideoUtilTest.java
@@ -16,17 +16,18 @@ class UploadVideoUtilTest {
     /**
      * 下载字幕调试
      */
-//    @Test
+    @Test
     void uploadTranslateVideo() throws InterruptedException, IOException {
         UploadVideoUtil uploadVideoUtil = new UploadVideoUtil();
-        File loadVideo = new File("C:\\视频\\项目\\20220911Mark_Zuckerberg_responds_to_Instagram_whistleblower_Frances_Haugen__Lex_Fridman_Podcast_Clips\\Mark_Zuckerberg_responds_to_Instagram_whistleblower_Frances_Haugen__Lex_Fridman_Podcast_Clips.mp4");
-        File ccFile = uploadVideoUtil.uploadTranslateVideo(loadVideo, false);
+        File loadVideo = new File("E:\\pythonProject\\whisper\\test_video\\3_Pieter_Levels_Programming__Viral_AI_Startups__and_Digital_Nomad_Life__Lex_Fridman_Podcast_#440.mp4");
+        File ccFile = new File("E:\\pythonProject\\whisper\\test_video\\3_Pieter_Levels_Programming__Viral_AI_Startups__and_Digital_Nomad_Life__Lex_Fridman_Podcast_#440.srt");
+//        File ccFile = uploadVideoUtil.uploadTranslateVideo(loadVideo, false);
         // 编辑视频
         // 复制视频到缓存目录下
         EditVideoUtil editVideoUtil = new EditVideoUtil();
         File subtitlesFile = editVideoUtil.encodedSubtitles(loadVideo, ccFile);
 //        File subtitlesFile = new File("C:\\视频\\项目\\20220904id Software origin story  John Carmack and Lex Fridman\\Subtitleid Software origin story  John Carmack and Lex Fridman.mp4");
         // 上传平台
-        uploadVideoUtil.uploadDouYinVideo("马克·扎克伯格回应Instagram告密者弗朗西斯·豪根 #访谈 #英语翻译 #大神程序员 #社交媒体 #元宇宙 #马克扎克伯格", subtitlesFile, new File(""));
+//        uploadVideoUtil.uploadDouYinVideo("马克·扎克伯格回应Instagram告密者弗朗西斯·豪根 #访谈 #英语翻译 #大神程序员 #社交媒体 #元宇宙 #马克扎克伯格", subtitlesFile, new File(""));
     }
 }
